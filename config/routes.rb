43bd23de95c6785d6a696678ebbe854ebf112480
Mike Wittenauer <mikewittenauer@me.com>
2022-01-30 19:28:48 -0500

    WRapup basic log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get "signup", to: "users#new"
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
+  get "logout", to: "sessions#destroy"
   resources :users, only: [:create, :show]
 end
