3afa6d5a26099820afcecc2165dcf2c845e7b12f
Mike Wittenauer <mikewittenauer@me.com>
2022-02-02 16:59:33 -0500

    Basic posts setup create and index

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,8 @@ module SessionsHelper
     def logged_in?
         session[:user_id] != nil
     end
+
+    def current_user
+        @current_user ||= User.find(session[:user_id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_133131) do
+ActiveRecord::Schema.define(version: 2022_02_02_023552) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "text"
+    t.date "schedule"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -21,4 +30,5 @@ ActiveRecord::Schema.define(version: 2022_01_30_133131) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
