5fc7b6e7f979c1d08083b6e1a72faa2844b6c1a1
Mike Wittenauer <mikewittenauer@me.com>
2022-02-06 14:40:02 -0500

    Solve the redis/sidekiq issu

--- app/jobs/submit_post_job.rb
+++ app/jobs/submit_post_job.rb
@@ -2,9 +2,8 @@ class SubmitPostJob < ApplicationJob
   queue_as :default
 
   def perform(post_id, post_updated_at)
-    # Simulates a long, time-consuming task
-    sleep 5
-    # Will display current time, milliseconds included
-    p "hello from HelloWorldJob #{Time.now().strftime('%F - %H:%M:%S.%L')}"
+    post = Post.find(post_id)
+    post.posted = true
+    post.save
   end
 end
