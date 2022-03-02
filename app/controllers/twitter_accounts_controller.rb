9b55f2677ee6fede2cfa3060f31cb7992d73d702
Mike Wittenauer <mikewittenauer@me.com>
2022-03-01 20:05:17 -0500

    Move token validation to the twitter account model

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -12,26 +12,14 @@ class TwitterAccountsController < ApplicationController
     def create
         @twitter_account = current_user.twitter_accounts.build(twitter_account_params)
 
-        configFile = YAML.load(File.open('config.yml').read)
-        puts configFile
-
-        client = Twitter::REST::Client.new do |config|
-            config.consumer_key = configFile["twitter"]["consumer_key"]
-            config.consumer_secret = configFile["twitter"]["consumer_secret"]
-            config.access_token = @twitter_account.access_token
-            config.access_token_secret = @twitter_account.access_token_secret
-        end
-
-        begin
-            client.friends
+        if @twitter_account.validate_tokens
             if @twitter_account.save
                 flash[:success] = "Twitter account created"
                 redirect_to posts_path
             else
                 render 'new'
             end
-        rescue Twitter::Error::Forbidden, Twitter::Error::Unauthorized
-            puts "Captured twitter exception"
+        else
             @twitter_account.errors.add(:base, "Twitter credentials not valid.")
             render 'new'
         end
--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -5,4 +5,20 @@ class TwitterAccount < ApplicationRecord
     validates :name, presence: true
     validates :access_token, presence: true
     validates :access_token_secret, presence: true
+
+    def validate_tokens
+        configFile = YAML.load(File.open('config.yml').read)
+        begin
+            client = Twitter::REST::Client.new do |config|
+                config.consumer_key = configFile["twitter"]["consumer_key"]
+                config.consumer_secret = configFile["twitter"]["consumer_secret"]
+                config.access_token = access_token
+                config.access_token_secret = access_token_secret
+            end
+            client.friends
+            true
+        rescue Twitter::Error::Forbidden, Twitter::Error::Unauthorized
+            false
+        end
+    end
 end
