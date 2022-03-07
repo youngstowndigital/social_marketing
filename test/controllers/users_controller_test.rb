227f79f29a72745b22e06e6c6363ca933358b2f8
Mike Wittenauer <mikewittenauer@me.com>
2022-03-07 14:23:35 -0500

    Require correct user for users page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,22 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:test)
+    @other_user = users(:other)
   end
 
   test "user page requires login" do
+    get user_path @user
+    assert_redirected_to root_path
+  end
+
+  test "get user page when logged in" do
+    login_user(@user, 'testpassword')
+    get user_path @user
+    assert_response :success
+  end
+
+  test "get user page requires the correct user logged in" do
+    login_user(@other_user, 'testpassword')
     get user_path @user
     assert_redirected_to root_path
   end
