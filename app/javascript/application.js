a7fc1fa1a065454619c121c7b3b31fda8f8968c8
Mike Wittenauer <mikewittenauer@me.com>
2022-03-13 11:16:26 -0400

    Fix jquery issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,21 +3,23 @@
 //= require jquery_ujs
 import "@hotwired/turbo-rails"
 import "controllers"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = false
 
-$(document).ready(function() {
-    console.log("ready!");
-
-    $('.posts .nav-item .posted').on("click", function() {
-        $('.posts .nav-item .active').removeClass('active');
-        $('#pending').css("display", "none");
-        $('#posted').css("display", "block");
-        $(this).addClass('active');
-    })
-
-    $('.posts .nav-item .scheduled').on("click", function() {
-        $('.posts .nav-item .active').removeClass('active');
-        $('#posted').css("display", "none");
-        $('#pending').css("display", "block");
-        $(this).addClass('active');
-    })
+window.addEventListener("load", () => {
+    $(function() {    
+        $('.posts .nav-item .posted').on("click", function() {
+            $('.posts .nav-item .active').removeClass('active');
+            $('#pending').css("display", "none");
+            $('#posted').css("display", "block");
+            $(this).addClass('active');
+        })
+    
+        $('.posts .nav-item .scheduled').on("click", function() {
+            $('.posts .nav-item .active').removeClass('active');
+            $('#posted').css("display", "none");
+            $('#pending').css("display", "block");
+            $(this).addClass('active');
+        })
+    });
 });
